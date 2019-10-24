29fcf7e85b3e1fc69d4babd5a65132a673444f55
Jose Geraldo de Carvalho Pereira <Jose Geraldo>
2019-10-24 13:01:45 -0300

    function ask for a job is ok

--- kv_web/src/main.rs
+++ kv_web/src/main.rs
@@ -1,27 +1,60 @@
 use actix_web::{HttpServer, App, web, Responder};
 use fasthash::city;
 use reqwest;
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize)]
+struct Job {
+    #[serde(default)]
+    id: String,     // this id is the same as tag_id (NOT queue_id)
+    status: String,
+    output: Option<String>,
+    created_at: String,
+    started_at: Option<String>,
+    ended_at: Option<String>, 
+    expires_after: String,
+}
 
 fn hello() -> impl Responder {
     "KVFinder Web"
 }
 
-fn get_id(tag_id: &String) -> Result<Option<String>, reqwest::Error> {
+fn get_queue_id(tag_id: &String) -> Result<Option<u32>, reqwest::Error> {
     let url = format!("http://0.0.0.0:8023/tag/{}", tag_id);
-    let mut ids: Vec<String> = reqwest::get(url.as_str())?.json()?;
-    //ids = vec!(String::from("1"),String::from("2"));
+    // ids because in theory could be more than one with the same tag, BUT if this happen there is an error 
+    let mut ids: Vec<u32> = reqwest::get(url.as_str())?.json()?; 
+        //ids = vec!(String::from("1"),String::from("2"));
     Ok(ids.pop())
+}
+
+fn get_job(tag_id: String) -> Result<Option<Job>, reqwest::Error> {
+    let queue_id = get_queue_id(&tag_id);
+    let job = |queue_id| {
+        let url = format!("http://0.0.0.0:8023/job/{}?fields=status,output,created_at,started_at,ended_at,expires_after", queue_id);
+        let mut j: Job = reqwest::get(url.as_str())?.json()?;
+        j.id = tag_id;
+        Ok(Some(j))
+    };
+ 
+    match queue_id {
+            Err(e) => Err(e),
+            Ok(None) => return Ok(None),
+            Ok(Some(queue_id)) => return job(queue_id), 
+    }
 }
 
 fn ask(id: web::Path<String>) -> impl Responder {
     let tag_id = id.into_inner();
-    let queue_id = get_id(&tag_id);
-    match queue_id {
-        Ok(None) => format!("{} not found in job queue", tag_id),
-        Ok(Some(id)) => format!("{} {}", tag_id, id),
+    let job = get_job(tag_id);
+    match job {
         Err(e) => format!("{:?}", e),
+        Ok(None) => format!("not found in job queue"),
+        Ok(Some(j)) => format!("{}", serde_json::to_string(&j).unwrap()),
     }
 }
+
+fn create() {}
 
 fn main() {
     let s = String::from("{data: atoms}");
@@ -33,6 +66,7 @@ fn main() {
         App::new()
             .route("/", web::get().to(hello))
             .route("/{id}", web::get().to(ask))
+            .route("/create", web::post().to(create))
     })
     .bind("127.0.0.1:8081")
     .expect("Cannot bind to port 8081")
