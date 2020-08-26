d3e21e314687f46f7d1e8fb2322d23a448341deb
jvsguerra <jvsguerra@gmail.com>
2020-08-26 17:55:23 -0300

    First template for KVFinder-web Tools

---
+++ client/KVFinder-web-tools/__init__.py
@@ -0,0 +1,99 @@
+'''
+PyMOL Demo Plugin
+
+The plugin resembles the old "Rendering Plugin" from Michael Lerner, which
+was written with Tkinter instead of PyQt.
+
+(c) Schrodinger, Inc.
+
+License: BSD-2-Clause
+'''
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+# Avoid importing "expensive" modules here (e.g. scipy), since this code is
+# executed on PyMOL's startup. Only import such modules inside functions.
+
+import os
+
+
+def __init_plugin__(app=None):
+    '''
+    Add an entry to the PyMOL "Plugin" menu
+    '''
+    from pymol.plugins import addmenuitemqt
+    addmenuitemqt('KVFinder-web Tools', run_plugin_gui)
+
+
+# global reference to avoid garbage collection of our dialog
+dialog = None
+
+
+def run_plugin_gui():
+    '''
+    Open our custom dialog
+    '''
+    global dialog
+
+    if dialog is None:
+        dialog = make_dialog()
+
+    dialog.show()
+
+
+def make_dialog():
+    # entry point to PyMOL's API
+    from pymol import cmd
+
+    # pymol.Qt provides the PyQt5 interface
+    from pymol.Qt import QtWidgets
+    from pymol.Qt.utils import loadUi
+    from pymol.Qt.utils import getSaveFileNameWithExt
+
+    # create a new Window
+    dialog = QtWidgets.QDialog()
+
+    # populate the Window from our *.ui file which was created with the Qt Designer
+    uifile = os.path.join(os.path.dirname(__file__), 'KVFinder-web.ui')
+    form = loadUi(uifile, dialog)
+
+    ######################
+    ### Dialog Buttons ###
+    ######################
+
+    # callback for the "Browse" button
+    def browse_filename():
+        filename = getSaveFileNameWithExt(
+            dialog, 'Save As...', filter='PNG File (*.png)')
+        if filename:
+            form.input_filename.setText(filename)
+
+
+
+    # hook up dialog buttons callbacks
+    form.button_run.clicked.connect(KVFinderWeb.run)
+    form.button_exit.clicked.connect(dialog.close)
+    form.button_restore.clicked.connect(KVFinderWeb.restore)
+    form.button_grid.clicked.connect(KVFinderWeb.show_grid)
+
+    return dialog
+
+
+class KVFinderWeb():
+    
+    # Methods
+    @staticmethod
+    def run():
+        id = 1 
+        print(f'\nRunning KVFinder-web for job id: {id}\n')
+
+    @staticmethod
+    def show_grid():
+        print('Showing Grid for current parameters ...\n')
+        pass
+
+    @staticmethod
+    def restore():
+        print('Restoring values ...\n')
+        pass
