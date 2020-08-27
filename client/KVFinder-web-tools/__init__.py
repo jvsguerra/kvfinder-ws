c681291cdb7848da8d85e699795f0542f950f240
jvsguerra <jvsguerra@gmail.com>
2020-08-27 14:02:31 -0300

    Transform plugin into class

--- client/KVFinder-web-tools/__init__.py
+++ client/KVFinder-web-tools/__init__.py
@@ -1,24 +1,10 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-PyMOL Demo Plugin
-
-The plugin resembles the old "Rendering Plugin" from Michael Lerner, which
-was written with Tkinter instead of PyQt.
-
-(c) Schrodinger, Inc.
-
-License: BSD-2-Clause
-'''
-
 from __future__ import absolute_import
 from __future__ import print_function
 
-# Avoid importing "expensive" modules here (e.g. scipy), since this code is
-# executed on PyMOL's startup. Only import such modules inside functions.
-
-import os
+import os, sys
 
 
 def __init_plugin__(app=None):
@@ -40,42 +26,48 @@ def run_plugin_gui():
     global dialog
 
     if dialog is None:
-        dialog = make_dialog()
+        dialog = KVFinderWeb().dialog
 
     dialog.show()
 
 
-def make_dialog():
-    # entry point to PyMOL's API
-    from pymol import cmd
+class KVFinderWeb(object):
 
-    # pymol.Qt provides the PyQt5 interface
-    from pymol.Qt import QtWidgets
-    from pymol.Qt.utils import loadUi
-    from pymol.Qt.utils import getSaveFileNameWithExt
-
-    # create a new Window
-    dialog = QtWidgets.QDialog()
-
-    # populate the Window from our *.ui file which was created with the Qt Designer
-    uifile = os.path.join(os.path.dirname(__file__), 'KVFinder-web.ui')
-    form = loadUi(uifile, dialog)
-
-    ######################
-    ### Dialog Buttons ###
-    ######################
-
-    # hook up dialog buttons callbacks
-    form.button_run.clicked.connect(KVFinderWeb.Buttons.run)
-    form.button_exit.clicked.connect(dialog.close)
-    form.button_restore.clicked.connect(KVFinderWeb.Buttons.restore)
-    form.button_grid.clicked.connect(KVFinderWeb.Buttons.show_grid)
-
-    return dialog
+    def __init__(self):
+        super(KVFinderWeb, self).__init__()
+        self.initUI()
 
 
-class KVFinderWeb():
-    
+    def initUI(self):
+        # entry point to PyMOL's API
+        from pymol import cmd
+
+        # pymol.Qt provides the PyQt5 interface
+        from PyQt5 import QtCore
+        from pymol.Qt import QtWidgets
+        from pymol.Qt.utils import loadUi
+        from pymol.Qt.utils import getSaveFileNameWithExt
+
+        # create a new Window
+        self.dialog = QtWidgets.QDialog()
+
+        # populate the Window from our *.ui file which was created with the Qt Designer
+        uifile = os.path.join(os.path.dirname(__file__), 'KVFinder-web.ui')
+        self.form = loadUi(uifile, self.dialog)
+
+        ######################
+        ### Dialog Buttons ###
+        ######################
+
+        # hook up dialog buttons callbacks
+        self.form.button_run.clicked.connect(self.Buttons.run)
+        self.form.button_exit.clicked.connect(self.dialog.close)
+        self.form.button_restore.clicked.connect(self.Buttons.restore)
+        self.form.button_grid.clicked.connect(self.Buttons.show_grid)
+
+        # return dialog
+
+
     class Buttons():
         
         # Methods
@@ -102,3 +94,11 @@ class KVFinderWeb():
             dialog, 'Save As...', filter='PNG File (*.png)')
         if filename:
             form.input_filename.setText(filename)
+
+
+if __name__ == '__main__':
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    dialog = KVFinderWeb().dialog
+    dialog.show()
+    sys.exit(app.exec_())
