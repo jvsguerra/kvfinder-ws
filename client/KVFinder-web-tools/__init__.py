7b8e41cd99f4726efdf456c7612c773f1316a11b
jvsguerra <jvsguerra@gmail.com>
2020-08-31 17:42:09 -0300

    Main Tab working

--- client/KVFinder-web-tools/__init__.py
+++ client/KVFinder-web-tools/__init__.py
@@ -7,17 +7,17 @@ from __future__ import print_function
 import os, sys
 
 # DEBUG flag
-DEBUG=True
+DEBUG = True
 
 
 # global reference to avoid garbage collection of our dialog
 dialog = None
 
 
-class Default(object):
+class _Default(object):
 
     def __init__(self):
-        super(Default, self).__init__()
+        super(_Default, self).__init__()
         #######################
         ### Main Parameters ###
         #######################
@@ -30,7 +30,8 @@ class Default(object):
         #######################
         ###  Search Space   ###
         #######################
-        # self.
+        # self.ligand_adjustment = False
+        # self.box_adjustment = False
         #######################
         ###     Results     ###
         #######################
@@ -52,50 +53,68 @@ def run_plugin_gui():
     global dialog
 
     if dialog is None:
-        dialog = KVFinderWeb()
+        dialog = PyMOLKVFinderWebTools()
 
     dialog.gui.show()
 
 
-class KVFinderWeb(object):
+class PyMOLKVFinderWebTools(object):
+    """
+    PyMOL KVFinder Web Tools
 
+    - creates Graphical User Interface in PyQt5 in PyMOL viewer
+    - defines and connects callbacks for Qt elements
+
+    """
     def __init__(self):
-        super(KVFinderWeb, self).__init__()
-        self.initGUI()
+        super(PyMOLKVFinderWebTools, self).__init__()
+        # Define Default Parameters
+        self._default = _Default()
+        # Initialize PyMOLKVFinderWebTools GUI
+        self.initialize_gui()
+        # Restore Default Parameters
         self.restore()
+        # Restore PDB input
+        self.refresh(self.gui.input)
 
-    def initGUI(self):
-        """
-        Qt elements are located in self.gui and self.form
-        """
 
+    def initialize_gui(self):
+        """
+        Qt elements are located in self.gui
+        """
         # entry point to PyMOL's API
         from pymol import cmd
-
         # pymol.Qt provides the PyQt5 interface
         from pymol.Qt import QtWidgets
         from pymol.Qt.utils import loadUi, getSaveFileNameWithExt
 
-        # create a new Window
+        # create a new QMainWindow
         self.gui = QtWidgets.QMainWindow()
 
-        # populate the Window from our *.ui file which was created with the Qt Designer
+        # populate the QMainWindow from our *.ui file
         uifile = os.path.join(os.path.dirname(__file__), 'KVFinder-web.ui')
         loadUi(uifile, self.gui)
 
-        #######################
-        ###   Gui Buttons   ###
-        #######################
+        ########################
+        ### Buttons Callback ###
+        ########################
 
-        # hook up dialog buttons callbacks
+        # hook up QMainWindow buttons callbacks
         self.gui.button_run.clicked.connect(self.run)
         self.gui.button_exit.clicked.connect(self.gui.close)
         self.gui.button_restore.clicked.connect(self.restore)
         self.gui.button_grid.clicked.connect(self.show_grid)
+        
+        # hook up Parameters button callbacks
+        self.gui.button_browse.clicked.connect(self.select_directory)
+        self.gui.refresh_input.clicked.connect(lambda: self.refresh(self.gui.input))
 
 
     ### Button Methods
     def run(self):
+        """
+        Callback for the "Run KVFinder-web" button
+        """
         id = 1 # dummy id value
         print(f'\nRunning KVFinder-web for job id: {id}\n')
         # TODO: 
@@ -105,17 +124,29 @@ class KVFinderWeb(object):
 
 
     def show_grid(self):
+        """
+        Callback for the "Show Grid" button
+        """
         print('Showing Grid for current parameters ...\n')
 
 
     def restore(self):
+        """
+        Callback for the "Restore Default Values" button
+        """
         print('Restoring values ...\n')
-        self.gui.base_name.setText(Default().base_name)
-        self.gui.probe_in.setValue(Default().probe_in)
-        self.gui.probe_out.setValue(Default().probe_out)
-        self.gui.volume_cutoff.setValue(Default().volume_cutoff)
-        self.gui.removal_distance.setValue(Default().removal_distance)
-        self.gui.output_dir_path.setText(Default().output_dir_path)
+        # Main tab
+        self.gui.base_name.setText(self._default.base_name)
+        self.gui.probe_in.setValue(self._default.probe_in)
+        self.gui.probe_out.setValue(self._default.probe_out)
+        self.gui.volume_cutoff.setValue(self._default.volume_cutoff)
+        self.gui.removal_distance.setValue(self._default.removal_distance)
+        self.gui.output_dir_path.setText(self._default.output_dir_path)
+
+        # Search Space Tab
+        self.gui.box_adjustment.setChecked(False)
+        self.gui.ligand_adjustment.setChecked(False)
+
         
         if DEBUG:
             print(f"Base Name: {self.gui.base_name.text()}")
@@ -124,18 +155,58 @@ class KVFinderWeb(object):
             print(f"Volume Cutoff: {self.gui.volume_cutoff.value()}")
             print(f"Removal Distance: {self.gui.removal_distance.value()}")
             print(f"Output Directory: {self.gui.output_dir_path.text()}")
+            print(f"Box adjustment: {self.gui.box_adjustment.isChecked()}")
+            print(f"Ligand adjustment: {self.gui.ligand_adjustment.isChecked()}")
 
-    # callback for the "Browse" button
-    def browse_filename():
-        filename = getSaveFileNameWithExt(
-            dialog, 'Save As...', filter='PNG File (*.png)')
-        if filename:
-            form.input_filename.setText(filename)
+    
+    def refresh(self, combo_box):
+        """
+        Callback for the "Refresh" button
+        """
+        from pymol import cmd
+        
+        combo_box.clear()
+        for item in cmd.get_names("all"):
+            if cmd.get_type(item) == "object:molecule" and \
+                item != "box" and \
+                item != "grid" and \
+                item != "cavities" and \
+                item != "residues" and \
+                item[-16:] != ".KVFinder.output" and \
+                item != "target_exclusive":
+                combo_box.addItem(item)
+        
+        return
+
+
+    def select_directory(self):
+        """ 
+        Callback for the "Browse ..." button
+        Open a QFileDialog to select a directory.
+        """
+        from PyQt5.QtWidgets import QFileDialog
+        from PyQt5.QtCore import QDir
+        
+        fname = QFileDialog.getExistingDirectory(caption='Choose Output Directory', directory=os.getcwd())
+
+        if fname:
+            fname = QDir.toNativeSeparators(fname)
+            if os.path.isdir(fname):
+                self.gui.output_dir_path.setText(fname)
+
+        return
+
+
+def KVFinderWebTools():
+    """ Debug KVFinderWebTools """
+    # TODO: transform it in a new tools without PyMOL later
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    dialog = PyMOLKVFinderWebTools()
+    dialog.gui.setWindowTitle('KVFinder-web Tools')
+    dialog.gui.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    from PyQt5.QtWidgets import QApplication
-    app = QApplication(sys.argv)
-    dialog = KVFinderWeb()
-    dialog.gui.show()
-    sys.exit(app.exec_())
+    KVFinderWebTools()
